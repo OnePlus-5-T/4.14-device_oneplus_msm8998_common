d495127a69212a0c82967aaeff6e5df6cc822193
Roberto Sartori <roberto.sartori.android@gmail.com>
2021-09-10 20:39:12 +0200

    Revert "msm8998-common: update include system/core/base -> system/libbase"
    This reverts commit a7fa52011ccd54084827304cff8a57a3dc6ca395.
    
    Wait for Android 12 for this.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -11,7 +11,7 @@ LOCAL_C_INCLUDES := \
     bootable/recovery/edify/include \
     bootable/recovery/otautil/include \
     bootable/recovery/updater/include \
-    system/libbase/include
+    system/core/base/include
 
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_oneplus
