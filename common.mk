3c283c0f3d6282e17a605feef3121cbedf97ae80
Roberto Sartori <roberto.sartori.android@gmail.com>
2021-11-23 20:54:40 +0100

    msm8998-common: boot ADSP and SLPI on kernel 4.14
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- common.mk
+++ common.mk
@@ -105,6 +105,11 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libvolumelistener
 
+# Common init scripts
+PRODUCT_PACKAGES += \
+    init.qcom.adspstart.sh \
+    init.qcom.slpistart.sh
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
---
+++ rootdir/bin/init.qcom.slpistart.sh
@@ -0,0 +1,4 @@
+#!/vendor/bin/sh
+
+echo 1 > /sys/kernel/boot_slpi/boot
+setprop vendor.qcom.slpiup 1
