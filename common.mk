bcf496f541b380bb66c8c8247d91f1bfac217e53
Roberto Sartori <roberto.sartori.android@gmail.com>
2022-02-13 20:46:50 +0100

    msm8998-common: use the Lineage Light HAL and disable the Sony one
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- common.mk
+++ common.mk
@@ -321,7 +321,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.sony
+    android.hardware.light@2.0-service.oneplus_msm8998
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
