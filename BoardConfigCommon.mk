b0dd6b01c68cc5970ca3c33d3a21261007423f8f
Roberto Sartori <roberto.sartori.android@gmail.com>
2022-02-13 15:20:28 +0100

    msm8998-common: remove useless BOARD_KERNEL_IMAGE_NAME definition
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -61,7 +61,6 @@ BOARD_KERNEL_CMDLINE += kpti=0
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 SOONG_CONFIG_NAMESPACES += sony_qti_kernel_headers
 SOONG_CONFIG_sony_qti_kernel_headers := version
